2c1dc9d1e45efc7297e20fee77be484d261099e2
Luc@s <lucasaquilapereira@gmail.com>
2022-12-16 22:33:39 -0300

    update v1

--- app.js
+++ app.js
@@ -1,3 +1,2 @@
 console.log('Receba!! Melhor do mundo!!');
 console.log('Rapaiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiz')
-console.log('oi')
