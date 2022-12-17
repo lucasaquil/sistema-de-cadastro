2dcb9e58563d564a42a09d5890a48e3c011a4a75
Luc@s <lucasaquilapereira@gmail.com>
2022-12-16 21:38:46 -0300

    uptade js

--- app.js
+++ app.js
@@ -1,1 +1,2 @@
 console.log('Receba!! Melhor do mundo!!');
+console.log('Rapaiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiz')
